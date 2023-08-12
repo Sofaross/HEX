daae96477c771640c43e137d871550cc605ca797
Sofaross <105317722+Sofaross@users.noreply.github.com>
2023-08-12 15:39:20 +0300

    Create addRow addColumn, createEmptyDataArray upgrade updateDataHex

--- src/model/HexEditor.java
+++ src/model/HexEditor.java
@@ -3,6 +3,7 @@ package model;
 import controller.ErrorHandler;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class HexEditor {
     private  byte[] data;
@@ -23,8 +24,9 @@ public class HexEditor {
             ErrorHandler.showError("Ошибка чтения файла: " + e.getMessage());
         }
     }
-    public void updateData(byte[] newData) {
-        this.data = newData;
+    public void createEmptyDataArray(int length) {
+        data = new byte[length];
+        Arrays.fill(data, (byte) 0);
     }
     public void setByte(int index, byte value) {
         if (data != null && index >= 0 && index < data.length) {
@@ -33,23 +35,14 @@ public class HexEditor {
             System.out.println("Ошибка: недопустимый индекс");
         }
     }
-    public byte getByte(int index) {
-        if (index >= 0 && index < data.length) {
-            return data[index];
-        } else {
-            System.out.println("Ошибка: недопустимый индекс");
-            return 0;
-        }
-    }
     public byte[] getData() {
         return data;
     }
-    public int getByteCount() {return data.length;}
+    public int getByteCount() {
+        return data.length;
+    }
     public File getFileName() {
-        if (file == null) {
-            return null;
-        }
-        return file;
+        return (file == null) ? null : file;
     }
     public void write(File file) {
         try (FileOutputStream outputStream = new FileOutputStream(file)) {
@@ -68,29 +61,41 @@ public class HexEditor {
         int dataSize = data.length;
         int totalCells = numRows * numColumns;
         int remainingCells = totalCells - dataSize;
+        addRow(remainingCells);
 
         for (int i = 0; i < data.length; i++) {
             int row = i / numColumns;
             int column = i % numColumns;
-            hexData[row][column] = String.format("%02X", data[i]);
+            if (data[i] != 0) {
+                hexData[row][column] = String.format("%02X", data[i]);
+            }
+            else hexData[row][column] = "00";
         }
+        return hexData;
+    }
 
-        if (remainingCells > 0) {
-            int lastRow = dataSize / numColumns;
-            int lastColumn = dataSize % numColumns;
+    public void addRow(int columnCount) {
+        byte[] zerosArray = new byte[columnCount];
+        Arrays.fill(zerosArray, (byte) 0);
 
-            for (int i = dataSize; i < totalCells; i++) {
-                int row = i / numColumns;
-                int column = i % numColumns;
+        byte[] combinedArray = new byte[data.length + zerosArray.length];
+        System.arraycopy(data, 0, combinedArray, 0, data.length);
+        System.arraycopy(zerosArray, 0, combinedArray, data.length, zerosArray.length);
+        data=combinedArray;
+    }
+    public void addColumn(int columnCount){
+        int insertCount = (data.length) / (columnCount);
+        int newDataLength = data.length + insertCount;
 
-                if (column <= lastColumn && row == lastRow) {
-                    hexData[row][column] = String.format("%02X", data[i - lastColumn]);
-                } else {
-                    hexData[row][column] = "00";
-                }
+        byte[] newArray = new byte[newDataLength];
+        int newIndex = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            newArray[newIndex++] = data[i];
+            if ((i + 1) % columnCount == 0) {
+                newArray[newIndex++] = 0;
             }
         }
-
-        return hexData;
+        data=newArray;
     }
 }
