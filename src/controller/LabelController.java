9d419a64fcde7fc20e879816b9f41d80c4fe9472
Sofaross <105317722+Sofaross@users.noreply.github.com>
2023-08-14 16:39:47 +0300

    Update cell and table information

--- src/controller/LabelController.java
+++ src/controller/LabelController.java
@@ -1,17 +1,20 @@
 package controller;
 
 import model.HexEditor;
-
-
 public class LabelController {
+    private static final int MAX_ASCII_VALUE = 127;
+    private static final int ASCII_OFFSET = 256;
+
     public static String byteCount(HexEditor hexEditor) {
         try {
             int byteCount = hexEditor.getByteCount();
             return ("Byte count: " + byteCount);
         } catch (Exception e) {
-            ErrorHandler.showError("Error updating byte count label");}
+            ErrorHandler.showError("Error updating byte count label");
+        }
         return "";
     }
+
     public static String hexToUnsignedDecimal(String hexByte) {
         if (hexByte != null && !hexByte.isEmpty()) {
             int decimalValue = Integer.parseInt(hexByte, 16);
@@ -19,21 +22,25 @@ public class LabelController {
         }
         return "";
     }
+
     public static String hexToSignedDecimal(String hexByte) {
         if (hexByte != null && !hexByte.isEmpty()) {
             int decimalValue = Integer.parseInt(hexByte, 16);
-            if (decimalValue > 127) {
-                decimalValue -= 256;
+            if (decimalValue > MAX_ASCII_VALUE) {
+                decimalValue -= ASCII_OFFSET;
             }
             return ("Unsigned: " + decimalValue);
         }
         return "";
     }
-    public static String fileName(HexEditor hexEditor){
-        if (hexEditor.getFileName() != null ) {
+
+    public static String fileName(HexEditor hexEditor) {
+        if (hexEditor.getFileName() != null) {
             return ("File: " + hexEditor.getFileName().getName());
-        } else return "";
+        }
+        return "";
     }
+
     public static String hexToAscii(String cellValue) {
         if (cellValue != null && !cellValue.isEmpty()) {
             try {
@@ -45,12 +52,12 @@ public class LabelController {
         }
         return "";
     }
-    private static char getAsciiChar(int decimalValue){
-        if (decimalValue >= 0 && decimalValue <=127){
+
+    private static char getAsciiChar(int decimalValue) {
+        if (decimalValue >= 0 && decimalValue <= MAX_ASCII_VALUE) {
             return (char) decimalValue;
-        } else{
+        } else {
             return '.';
         }
     }
-
 }
