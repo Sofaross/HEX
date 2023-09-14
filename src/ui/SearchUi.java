22afb433e88ed8fadf81117fd0e5263c902033b1
Sofaross <sofaross228@gmail.com>
2023-09-14 15:26:38 +0300

    Translated the title in lines into English
    Signed-off-by: Sofaross <sofaross228@gmail.com>

--- src/ui/SearchUi.java
+++ src/ui/SearchUi.java
@@ -9,13 +9,15 @@ public class SearchUi extends JPanel {
     public SearchUi(FindController controller) {
         setLayout(new GridLayout(3, 1));
         JTextField findTextField = new JTextField();
-        add(new JLabel("Найти:"));
+        add(new JLabel("Find:"));
         add(findTextField);
 
-        int result = JOptionPane.showConfirmDialog(null, this, "Найти", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        int result = JOptionPane.showConfirmDialog(null, this, "Find", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
         if (result == JOptionPane.OK_OPTION) {
-            controller.parseSearchText(findTextField.getText());
-
+            String text = findTextField.getText().trim();
+            if (!text.isEmpty()) {
+                controller.parseSearchText(findTextField.getText());
+            }
         }
 
         setVisible(false);
